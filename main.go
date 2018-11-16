415172e9e34ef60c1277371852153b6a09612b67
Xiong <347964202@qq.com>
2018-11-16 14:45:30 +0800

    Finally

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
-import(
+import (
 	"os"
 
 	"github.com/Xiongzj5/cloud-go/routes"
 	flag "github.com/spf13/pflag"
 )
 
-const ( 
-	//Default host configuration
+const (
+	// Default host configuration
 	defaultHost string = "localhost"
-	//Default port configuration
+	// Default port configuration
 	defaultPort string = "8080"
 )
 
@@ -18,15 +18,21 @@ func main() {
 	// Get the router from routes model
 	r := routes.Router()
 
-	//Trying to fetch the host configuration in environment variables
-	prot = os.Getenv("PORT")
-	if len(port) == 0{
+	// Trying to fetch the host configuration in environment variables
+	host := os.Getenv("HOST")
+	if len(host) == 0 {
+		host = defaultHost
+	}
+
+	// Trying to fetch the port configuration in environment variables
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
 		port = defaultPort
 	}
 
 	// Trying to fetch the host & port configuration in the flags
-	hostFlag := flag.StringP("hostname", "h", defaultHost, "The host to deploy at")
-	portFlag := flag.StringP("port", "p", defaultPort, "The port for listening")
+	hostFlag := flag.StringP("hostname", "h", defaultHost, "The host to deploy at.")
+	portFlag := flag.StringP("port", "p", defaultPort, "The port for listening.")
 	flag.Parse()
 	if len(*hostFlag) != 0 {
 		host = *hostFlag
@@ -34,6 +40,7 @@ func main() {
 	if len(*portFlag) != 0 {
 		port = *portFlag
 	}
+
 	// Run the server @host:port
 	r.Run(host + ":" + port)
 }
